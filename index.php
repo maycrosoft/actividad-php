3a17b2072432a102e8a6b60dd3438a570ecbb9f9
mayra coronel <mayralcoronel@gmail.com>
2021-07-04 20:35:41 -0300

    actividad utilizando php

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+echo "Nombre: Mayra <br>";
+echo "Apellido: Coronel <br>";
+echo "Edad: 20 <br>";
+echo "Hobbie: leer <br>";
+echo "Editor de código preferido: Atom <br>";
+echo "Sistema operativo que utiliza: Windows 7 <br>";
+?>
